4877b30afd1ed37a9117a166498596cae443d633
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-03-28 00:03:36 -0400

    starts put routes. More work needed.

--- routes/api.js
+++ routes/api.js
@@ -14,6 +14,8 @@ router.get('/exercise', (req, res) => {
 
 //======== API ROUTES ========
 
+//======== GET routes ========
+
 router.get('/api/transaction', (req, res) => {
   Transaction.find({})
     .then(dbTransaction => {
@@ -33,6 +35,24 @@ router.get('/api/workouts/range', (req, res) => {
     .catch(err => {
       res.status(400).json(err);
     });
+});
+
+
+// ======== PUT routes ========
+
+router.put('/api/workouts/:id', (req, res) => {
+  console.log(req.params.id);
+});
+
+router.put('/api/workouts', ({ body }, res) => {
+  res.json(body);
+  // Transaction.create(body)
+  //   .then(dbTransaction => {
+  //     res.json(dbTransaction);
+  //   })
+  //   .catch(err => {
+  //     res.status(400).json(err);
+  //   });
 });
 
 module.exports = router;
